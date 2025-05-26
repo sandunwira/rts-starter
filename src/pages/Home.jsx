6374da223d16830edd7c5e31d4ea30e9e5e0c482
sandunwira <79461263+sandunwira@users.noreply.github.com>
2025-05-26 21:44:42 +0530

    Added Top Right Right arrow icon from Tabler Icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,12 +22,13 @@ function HomePage() {
 						Edit <code className="font-mono bg-zinc-200 px-1 py-0.5 text-zinc-800 font-light">src/pages/Home.jsx</code> and save to reload.
 					</p>
 					<a
-						className="text-sm flex flex-row items-center justify-center rounded bg-cyan-500 text-zinc-50 hover:bg-cyan-600 px-4 py-2 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2"
+						className="text-sm flex flex-row items-center justify-center gap-1 rounded bg-cyan-500 text-zinc-50 hover:bg-cyan-600 px-4 py-2 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2"
 						href="https://reactjs.org"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
 						Learn React
+						<svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-up-right"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M17 7l-10 10" /><path d="M8 7l9 0l0 9" /></svg>
 					</a>
 					<p className="mt-8 text-sm text-green-600 border border-green-600 rounded-xs px-4 py-2 bg-green-200">
 						<span className="font-bold">Congratulations!</span> You have successfully configured Tailwind CSS in your React application
